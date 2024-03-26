3cf006cf77f9f64705966dee6cc2d37f0fcd7cc1
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-03-25 23:38:43 -0300

    Add files via upload

---
+++ TestMe.java
@@ -0,0 +1,23 @@
+public class TestMe {
+
+    // multiplies two integers and returns the result.
+    public int multiply(int a, int b)  {
+
+        // fill in multiply code here
+        return a*b;
+
+    } // end multiply
+
+    // divides two doubles and returns the result.
+    public double divide(double a, double b) {
+
+        return 0.0;
+    } // end divide
+    
+    // determines wether the input is even and returns true or false
+    public boolean isEven(int a) {
+
+        return false;
+    } // end isEven
+    
+} // end class TestMe
